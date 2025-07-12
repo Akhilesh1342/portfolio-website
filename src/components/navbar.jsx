fc7f6d5fdaf80d20570a9901faf9a6f4fc73bd62
akhilesh <sit24ad048@sairamtap.edu.in>
2025-07-12 23:02:54 +0530

    sup bro

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,14 @@ const Navbar = () => {
   return (
     <nav className="bg-white shadow-md fixed w-full z-20 top-0 left-0">
       <div className="max-w-7xl mx-auto px-4 py-4 flex justify-between items-center">
-        <h1 className="text-2xl font-bold text-gray-600">A-K</h1>   
+        <a href="/">
+    <img
+      src="/a-favicon.png"
+      alt="A-Stacks Logo"
+      className="h-12 w-auto cursor-pointer"
+    />
+  </a>
+   
         <ul className="hidden md:flex space-x-6 text-gray-700 font-medium">
           <li><a href="#about" className="hover:text-indigo-600">About</a></li>
           <li><a href="#skills" className="hover:text-indigo-600">Skills</a></li>
