888236764a5e24c144eb3d99ca24a4d0000ce719
akhilesh <sit24ad048@sairamtap.edu.in>
2025-07-14 00:34:32 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,11 @@ import Contact from './components/contacts'
 function App() {
   return (
     <div className="font-poppins">
+      <div className="absolute top-0 -z-10 w-full h-[100vh] overflow-hidden">
+        <div className="w-[30rem] h-[30rem] bg-orange-200 rounded-full absolute animate-pulse opacity-30 top-10 left-10 blur-3xl" />
+        <div className="w-[20rem] h-[20rem] bg-yellow-200 rounded-full absolute animate-ping opacity-20 top-[15rem] right-20 blur-2xl" />
+      </div>
+
       <Navbar />
       <Hero />
       <About />
