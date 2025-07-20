7ff48118640b2df554f26c2ee9a85c3c611ee059
akhilesh <sit24ad048@sairamtap.edu.in>
2025-07-20 16:47:40 +0530

    part 4

--- src/components/contacts.jsx
+++ src/components/contacts.jsx
@@ -5,14 +5,11 @@ const Contact = () => {
         <h2 className="text-3xl font-bold text-indigo-600 mb-6">Contact</h2>
         <p className="text-gray-700 text-lg mb-4">Let’s connect and build cool stuff together. Drop a message or hit me up directly!</p>
         <p className="text-indigo-600 font-medium text-lg">📧 sit24ad048@sairamtap.edu.in</p>
-        <a
-            href="https://github.com/Akhilesh1342"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-xl font-medium text-blue-600 hover:underline hover:text-blue-800 transition"
-        >
-          🔗 github.com/Akhilesh1342
-</a>
+        <p>
+    <a href="https://github.com/Akhilesh1342" target="_blank" rel="noopener noreferrer" className="text-blue-600 underline">
+      github.com/Akhilesh1342
+    </a>
+  </p>
         <p>
     <a href="https://www.linkedin.com/in/akhilesh-m" target="_blank" rel="noopener noreferrer" className="text-blue-600 underline">
       linkedin.com/in/akhilesh-m
